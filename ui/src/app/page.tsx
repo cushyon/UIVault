20ee481188a40eba1fab74483f6cf88b7c30e8dc
nada-r <nadarzer@gmail.com>
2025-06-16 22:39:18 +0200

    add a button to checks vault if someone end on homepage

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,8 +1,14 @@
-export default async function HomePage() {
+import Link from "next/link";
 
+export default async function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full gap-6 bg-main-bg">
-      Home
+      <Link
+        href="/vaults"
+        className="flex items-center justify-center px-6 py-3 text-lg font-medium text-white transition-colors rounded-lg from-primary to-primary/80 hover:from-primary/90 hover:to-primary/70  bg-[linear-gradient(90deg,#091BCD_0%,#123FFC_35%,#0B3FE8_64%,#4571F4_100%)]"
+      >
+        Go to Vaults
+      </Link>
     </div>
   );
 }

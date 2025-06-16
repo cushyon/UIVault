a2cf75d3825bc2a666903205b6306c9e31caa0e9
nada-r <nadarzer@gmail.com>
2025-06-16 14:16:54 +0200

    rm unused imports

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { SiX, SiTelegram } from "react-icons/si";
-import { Logo } from "@/components/ui/logo";
 import Image from "next/image";
 
 export function Footer() {
